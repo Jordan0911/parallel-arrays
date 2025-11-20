0ac9b912402d88be85a993d3d944ad4073107c4e
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2025-11-20 10:50:42 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace parallel_arrays
+{
+    internal class Program
+    {
+        static string[]  Inventoryprinter(int[] array1, string[] array2)
+        {
+            string[] result = new string[array1.Length];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                result[i] = array1[i] + array2[i];
+            }
+            return result;
+ 
+        }
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            int[] quantity = new int[5];
+            string[] names = { "pear"; "sugar")
+        }
+    }
+}
